a9e1669641a8e8d0af010b2d6bde4eda1a9d2303
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-31 15:05:14 +0200

    added Milestone 1

--- main.js
+++ main.js
@@ -13,7 +13,6 @@
 // Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
 // Consigli del giorno:
 // Ragioniamo come sempre a step. Prima scriviamo nei commenti la logica in italiano e poi traduciamo in codice. console.log() è nostro amico. Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
-// :githubparrot: :githubparrot: :githubparrot: Buon lavoro!
 
 
 
@@ -81,34 +80,41 @@ const posts = [
 
 
 
-// <!-- post di esempio/template, da togliere/commentare e generare da JS -->
-// <!-- <div class="post">
-//     <div class="post__header">
-//         <div class="post-meta">                    
-//             <div class="post-meta__icon">
-//                 <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-//             </div>
-//             <div class="post-meta__data">
-//                 <div class="post-meta__author">Phil Mangione</div>
-//                 <div class="post-meta__time">4 mesi fa</div>
-//             </div>                    
-//         </div>
-//     </div>
-//     <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-//     <div class="post__image">
-//         <img src="https://unsplash.it/600/300?image=171" alt="">
-//     </div>
-//     <div class="post__footer">
-//         <div class="likes js-likes">
-//             <div class="likes__cta">
-//                 <a class="like-button  js-like-button" href="#" data-postid="1">
-//                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-//                     <span class="like-button__label">Mi Piace</span>
-//                 </a>
-//             </div>
-//             <div class="likes__counter">
-//                 Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-//             </div>
-//         </div> 
-//     </div>            
-// </div> -->
+let postContainer = document.querySelector("#container");
+
+posts.forEach((post) => {
+    postContainer.innerHTML += 
+
+
+`<div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${post.author.image}">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${post.author.name}</div>
+                <div class="post-meta__time">4 mesi fa</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${post.content}</div>
+    <div class="post__image">
+        <img src="https://unsplash.it/300/300?image" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+            </div>
+        </div> 
+    </div>            
+</div>`
+
+});
